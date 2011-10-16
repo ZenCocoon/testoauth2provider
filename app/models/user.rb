f3d9d1b0f33a3087a6007ea4f2c3bf76496f14e7
Sébastien Grosjean <public@zencocoon.com>
2011-10-16 14:38:45 +0300

    Install Devise OAuth2 Providable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :oauth2_providable,
+         :oauth2_refresh_token_grantable,
+         :oauth2_authorization_code_grantable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TestOAuth2Provider::Application.routes.draw do
   devise_for :users
+  mount Devise::Oauth2Providable::Engine => '/oauth2'
 
   get "home/index"
 
