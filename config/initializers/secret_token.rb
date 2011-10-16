39ec2a7659238431687fea008db7e1af17835098
Sébastien Grosjean <public@zencocoon.com>
2011-10-16 14:13:04 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestOAuth2Provider::Application.config.secret_token = 'bccaa0b3f078215ee5b1fbe15d53fc014c37c2d2e3ce7db1325bd2f1be7a38a1a78b508311d39e1372a683b7dee01d7a157e5ade8d3ef99a72809ecbef695625'
